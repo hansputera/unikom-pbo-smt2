c931260942c758eec33d4baed4409f8df1635f1a
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2026-04-08 19:21:00 +0700

    feat: added bools, chars, and reassign
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- pbo1/src/main/java/id/hanifu/Main.java
+++ pbo1/src/main/java/id/hanifu/Main.java
@@ -2,17 +2,40 @@ package id.hanifu;
 
 public class Main {
     public static void main(String[] args) {
+        // Numbers
         byte thisByte = 100;
         short thisShort = 1000;
         int thisInteger = 10000;
         long thisLong = 100000000;
         long thisLong2 = 100000000L;
+        float thisFloat = 10.10f;
+        double thisDouble = 20.20;
 
         Main.print("data byte", thisByte);
         Main.print("short", thisShort);
         Main.print("integer", thisInteger);
         Main.print("long", thisLong);
         Main.print("long2", thisLong2);
+        Main.print("float", (long) thisFloat);
+        Main.print("double", (long) thisDouble);
+
+        // Booleans
+        boolean thisBool = true;
+        boolean thisBool2 = false;
+
+        System.out.println("Bool " + thisBool);
+        System.out.println("Bool 2 " + thisBool2);
+
+        // Chars
+        char abjad = 'P';
+        System.out.println("Char " + abjad);
+
+        abjad = 'E';
+        System.out.println("Char (changed) " + abjad);
+
+        // Numbers (reassign)
+        thisByte = 50;
+        Main.print("data byte", thisByte);
     }
 
     protected static void print(String id, long val) {
